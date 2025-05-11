da3ab47c6d1c53c7574b0708f24886837e2c736a
max <justmax.xex@mail.ru>
2025-05-11 15:30:29 +0200

    refactor: move Header and layout to Root component and restructure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 import { Header } from "./components/Header";
 
@@ -10,14 +10,27 @@ import { ProductsPage } from "./pages/ProductsPage";
 import { SummaryPage } from "./pages/SummaryPage";
 import { Cart } from "./pages/CartPage";
 
+const Root: React.FC = () => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
 export const App: React.FC = () => {
   return (
     <CartProvider>
-      <Header />
       <Routes>
-        <Route path="/" element={<ProductsPage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/summary" element={<SummaryPage />} />
+        <Route path="/" element={<Root />}>
+          <Route index element={<ProductsPage />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="summary" element={<SummaryPage />} />
+        </Route>
+        <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </CartProvider>
   );
